dfaca9d8ebd0ecafc9c787d34b7b3352bc5ed56d
laoxla <oasiyanbola@gmail.com>
2018-06-19 13:59:36 -0400

    Added signup, login, logout views

--- accounts/views.py
+++ accounts/views.py
@@ -23,11 +23,19 @@ def signup(request):
 
 
 def login(request):
-
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'username or password is invalid.'})
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    # TODO Need to route to home page
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
 
